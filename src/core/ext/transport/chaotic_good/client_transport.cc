985444ff727eeaec8b8c28385b2d4ebdbc1961eb
Nana Pang <ladynana@google.com>
2023-07-19 18:26:49 +0000

    Initial changes.

---
+++ src/core/ext/transport/chaotic_good/client_transport.cc
@@ -0,0 +1,103 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <grpc/support/port_platform.h>
+
+#include "src/core/ext/transport/chaotic_good/client_transport.h"
+
+#include <string>
+#include <tuple>
+
+#include "absl/status/statusor.h"
+#include "absl/types/variant.h"
+#include "frame.h"
+
+#include <grpc/event_engine/event_engine.h>
+#include <grpc/slice.h>
+
+#include "src/core/ext/transport/chaotic_good/frame.h"
+#include "src/core/ext/transport/chaotic_good/frame_header.h"
+#include "src/core/ext/transport/chttp2/transport/hpack_encoder.h"
+#include "src/core/lib/promise/activity.h"
+#include "src/core/lib/promise/detail/basic_join.h"
+#include "src/core/lib/promise/event_engine_wakeup_scheduler.h"
+#include "src/core/lib/promise/join.h"
+#include "src/core/lib/promise/loop.h"
+#include "src/core/lib/promise/poll.h"
+#include "src/core/lib/slice/slice.h"
+#include "src/core/lib/slice/slice_buffer.h"
+#include "src/core/lib/slice/slice_internal.h"
+#include "src/core/lib/transport/promise_endpoint.h"
+
+namespace grpc_core {
+namespace chaotic_good {
+
+ClientTransport::ClientTransport(const ChannelArgs& channel_args,
+                                 PromiseEndpoint control_endpoint,
+                                 PromiseEndpoint data_endpoint) {
+  HPackCompressor hpack_compressor;
+  writer_ = MakeActivity(
+      Loop([this, &hpack_compressor, &control_endpoint, &data_endpoint]() {
+        return Seq(this->outgoing_frames_.Next(), [&hpack_compressor,
+                                                   &control_endpoint,
+                                                   &data_endpoint](
+                                                      FrameInterface* frame) {
+          auto control_endpoint_buffer = frame->Serialize(&hpack_compressor);
+          FrameHeader frame_header =
+              FrameHeader::Parse(
+                  reinterpret_cast<const uint8_t*>(grpc_slice_to_c_string(
+                      control_endpoint_buffer.c_slice_buffer()->slices[0])))
+                  .value();
+          SliceBuffer data_endpoint_buffer;
+          // Handle data endpoint buffer based on the frame type.
+          switch (frame_header.type) {
+            case FrameType::kSettings:
+              // No data will be sent on data endpoint;
+              break;
+            case FrameType::kFragment: {
+              uint8_t message_padding_size = frame_header.message_padding;
+              std::string message_padding(message_padding_size, '0');
+              Slice slice(grpc_slice_from_cpp_string(message_padding));
+              data_endpoint_buffer.Append(std::move(slice));
+              uint8_t message_size = frame_header.message_length;
+              auto message = std::move(dynamic_cast<ClientFragmentFrame*>(frame)->message);
+              GPR_ASSERT(message_size == message->payload()->Length());
+              message->payload()->MoveFirstNBytesIntoSliceBuffer(message_size, data_endpoint_buffer);
+              std::cout<<"message length " << message_size;
+              break;
+            }
+            case FrameType::kCancel:
+              // No data will be sent on data endpoint;
+              break;
+          }
+          return Seq(
+              Join(control_endpoint.Write(std::move(control_endpoint_buffer)),
+                   data_endpoint.Write(std::move(data_endpoint_buffer))),
+              [](std::tuple<absl::StatusOr<SliceBuffer>,
+                            absl::StatusOr<SliceBuffer>>
+                     ret) -> Poll<absl::variant<Continue, absl::Status>> {
+                if (!(std::get<0>(ret).ok() && std::get<1>(ret).ok())) {
+                  return absl::InternalError("Endpoint Write failed.");
+                }
+                return Continue();
+              });
+        });
+      }),
+      EventEngineWakeupScheduler(
+          grpc_event_engine::experimental::CreateEventEngine()),
+      [](absl::Status status) -> absl::Status { return status; });
+}
+
+}  // namespace chaotic_good
+}  // namespace grpc_core
---
+++ src/core/ext/transport/chaotic_good/client_transport.h
@@ -0,0 +1,101 @@
+// Copyright 2022 gRPC authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CLIENT_TRANSPORT_H
+#define GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CLIENT_TRANSPORT_H
+
+#include <grpc/support/port_platform.h>
+
+#include <stdint.h>
+
+#include <initializer_list>
+#include <utility>
+
+#include "absl/base/thread_annotations.h"
+#include "absl/status/status.h"
+
+#include "src/core/ext/transport/chaotic_good/frame.h"
+#include "src/core/lib/channel/channel_args.h"
+#include "src/core/lib/gprpp/sync.h"
+#include "src/core/lib/promise/activity.h"
+#include "src/core/lib/promise/loop.h"
+#include "src/core/lib/promise/mpsc.h"
+#include "src/core/lib/promise/pipe.h"
+#include "src/core/lib/promise/poll.h"
+#include "src/core/lib/promise/seq.h"
+#include "src/core/lib/transport/promise_endpoint.h"
+#include "src/core/lib/transport/transport.h"
+
+namespace grpc_core {
+namespace chaotic_good {
+
+class ClientTransport {
+ public:
+  ClientTransport(const ChannelArgs& channel_args,
+                  PromiseEndpoint control_endpoint_,
+                  PromiseEndpoint data_endpoint_);
+  auto AddStream(CallArgs call_args) {
+    // At this point, the connection is set up.
+    // Start sending data frames.
+    ClientFragmentFrame initial_frame;
+    initial_frame.headers = std::move(call_args.client_initial_metadata);
+    initial_frame.end_of_stream = false;
+    {
+      MutexLock lock(&mu_);
+      initial_frame.stream_id = next_stream_id_++;
+    }
+    uint32_t stream_id = initial_frame.stream_id;
+    MpscSender<FrameInterface*> outgoing_frames =
+        this->outgoing_frames_.MakeSender();
+    outgoing_frames.Send(dynamic_cast<FrameInterface*>(&initial_frame));
+    return Loop([&call_args, &stream_id, this]() -> LoopCtl<absl::Status> {
+      if (call_args.client_to_server_messages != nullptr) {
+        ClientFragmentFrame frame;
+        frame.stream_id = stream_id;
+        MpscSender<FrameInterface*> outgoing_frames =
+            this->outgoing_frames_.MakeSender();
+        auto next_message = call_args.client_to_server_messages->Next()();
+        auto message = std::move(next_message.value());
+        if (message.has_value()) {
+          frame.message = std::move(message.value());
+          frame.end_of_stream = false;
+        } else {
+          frame.end_of_stream = true;
+        }
+        if (frame.end_of_stream) {
+          return absl::OkStatus();
+        }
+        std::cout << "\n send message in Loop: " << frame.message->payload()->JoinIntoString();
+        outgoing_frames.Send(dynamic_cast<FrameInterface*>(&frame));
+        return Continue();
+      }
+      return absl::OkStatus();
+    });
+  }
+
+ private:
+  // Max buffer is set to 4, so that for stream writes each time it will queue
+  // at most 2 frames.
+  MpscReceiver<FrameInterface*> outgoing_frames_ =
+      MpscReceiver<FrameInterface*>(4);
+  Mutex mu_;
+  uint32_t next_stream_id_ ABSL_GUARDED_BY(mu_) = 1;
+  ActivityPtr writer_;
+  ActivityPtr reader_;
+};
+
+}  // namespace chaotic_good
+}  // namespace grpc_core
+
+#endif  // GRPC_SRC_CORE_EXT_TRANSPORT_CHAOTIC_GOOD_CLIENT_TRANSPORT_H
